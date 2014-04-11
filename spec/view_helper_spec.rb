53a1db2360043895e15c2b6844ee2e0919a7ef17
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-04-11 18:56:03 +0200

    Add specs for… something

---
+++ spec/view_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'lmgtfy_helper/view_helper'
+
+describe LetMeGoogleThatForYouHelper::ViewHelpers do
+
+  describe '#something_to_test' do
+
+    it 'wokrs just fine' do
+      expect(subject.something_to_test(0)).to be_true
+    end
+
+    it 'for both cases' do
+      expect(subject.something_to_test(1)).to be_false
+    end
+  end
+end
