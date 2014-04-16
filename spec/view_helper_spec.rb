d0bb47d9137eccb7d4fe3e32ebb12f772f4df7c3
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-04-16 13:57:14 +0200

    add travis file

--- spec/view_helper_spec.rb
+++ spec/view_helper_spec.rb
@@ -3,7 +3,6 @@ require 'lmgtfy_helper/view_helper'
 describe LetMeGoogleThatForYouHelper::ViewHelpers do
 
   describe '#something_to_test' do
-
     it 'wokrs just fine' do
       expect(subject.something_to_test(0)).to be_true
     end
